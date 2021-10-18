cb474aba80cb25054079392437ad22ce59eda738
IMS212 <31803019+IMS212@users.noreply.github.com>
2021-10-18 09:23:05 -0700

    Fix errors

--- src/main/java/net/irisshaders/lilybot/LilyBot.kt
+++ src/main/java/net/irisshaders/lilybot/LilyBot.kt
@@ -4,9 +4,10 @@ import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.utils.env
 import dev.kord.common.entity.Snowflake
 import net.irisshaders.lilybot.commands.*
+import net.irisshaders.lilybot.utils.BOT_TOKEN
 
-    suspend fun main() {
-        val bot = ExtensibleBot(TOKEN) {
+suspend fun main() {
+        val bot = ExtensibleBot(BOT_TOKEN) {
             chatCommands {
                 // Enable chat command handling
                 enabled = true
