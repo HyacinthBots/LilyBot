b10d593f3532dc804e72c41bcc5faf8373181308
NoComment1105 <NoComment1105@outlook.com>
2021-11-21 08:04:08 +0000

    Add manual reporting

--- src/main/kotlin/net/irisshaders/lilybot/commands/Report.kt
+++ src/main/kotlin/net/irisshaders/lilybot/commands/Report.kt
@@ -3,13 +3,19 @@
 package net.irisshaders.lilybot.commands
 
 import com.kotlindiscord.kord.extensions.DISCORD_RED
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.converters.impl.long
+import com.kotlindiscord.kord.extensions.commands.converters.impl.string
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.ephemeralMessageCommand
+import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
 import com.kotlindiscord.kord.extensions.types.edit
 import com.kotlindiscord.kord.extensions.types.respond
 import dev.kord.core.behavior.channel.GuildMessageChannelBehavior
 import dev.kord.core.behavior.channel.createEmbed
 import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.behavior.getChannelOf
+import dev.kord.core.entity.channel.GuildMessageChannel
 import kotlinx.datetime.Clock
 import net.irisshaders.lilybot.utils.ACTION_LOG
 import net.irisshaders.lilybot.utils.GUILD_ID
@@ -30,7 +36,7 @@ class Report : Extension() {
                 respond {
                     content = "Message reported to staff"
                 }
-                actionLog.createMessage { content = "<@&${MODERATORS.value}> get summoned" }
+                actionLog.createMessage { content = "<@&${MODERATORS.value}>" }
                 actionLog.createEmbed {
                     color = DISCORD_RED
                     title = "Message reported!"
@@ -53,5 +59,35 @@ class Report : Extension() {
             }
 
         }
+        ephemeralSlashCommand(::ManualReportArgs) {
+            name = "manual-report"
+            description = "Manually report a message"
+
+            action {
+                val actionLog = guild!!.getChannelOf<GuildMessageChannel>(ACTION_LOG)
+
+                respond {
+                    content = "Message reported to staff"
+                }
+                actionLog.createMessage { content = "`<@&${MODERATORS.value}>`"}
+                actionLog.createEmbed {
+                    color = DISCORD_RED
+                    title = "Message Manually Reported!"
+
+                    field {
+                        value = "**Message Link:** ${arguments.message}"
+                        inline = true
+                    }
+                    field {
+                        value = "**Reported by:** ${user.asUser().tag}"
+                        inline = false
+                    }
+                    timestamp = Clock.System.now()
+                }
+            }
+        }
+    }
+    inner class ManualReportArgs : Arguments() {
+        val message by string("message", "Link to the message to report")
     }
 }
