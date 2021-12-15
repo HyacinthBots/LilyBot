a8103c873eb4df05913f90abaf75aaa020ff7913
NoComment1105 <NoComment1105@outlook.com>
2021-12-15 05:28:43 +0000

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 pluginManagement {
     plugins {
         // Update this in libs.version.toml when you change it here
-        kotlin("jvm") version "1.6.0"
-        kotlin("plugin.serialization") version "1.6.0"
+        kotlin("jvm") version "1.6.10"
+        kotlin("plugin.serialization") version "1.6.10"
 
         // Update this in libs.version.toml when you change it here
 
-        id("com.github.johnrengelman.shadow") version "7.1.0"
+        id("com.github.johnrengelman.shadow") version "7.1.1"
     }
 }
 
