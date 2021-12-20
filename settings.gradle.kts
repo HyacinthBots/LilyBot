4eff47f9abd298f96a20f41e216c59edd6accec2
NoComment1105 <NoComment1105@outlook.com>
2021-12-20 11:32:57 +0000

    Please for the love of god stop crashing my IDE

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     plugins {
         // Update this in libs.version.toml when you change it here
-        kotlin("jvm") version "1.6.10"
-        kotlin("plugin.serialization") version "1.6.10"
+        kotlin("jvm") version "1.6.0"
+        kotlin("plugin.serialization") version "1.6.0"
 
         // Update this in libs.version.toml when you change it here
 
