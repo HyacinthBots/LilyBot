9d033d8b402e93656ee21e8d917c71fcedcf781d
Miss-Corruption <Miss_Corruption@protonmail.com>
2021-12-22 13:13:31 +0100

    Moved report creation to method

--- src/main/kotlin/net/irisshaders/lilybot/commands/Report.kt
+++ src/main/kotlin/net/irisshaders/lilybot/commands/Report.kt
@@ -14,12 +14,16 @@ import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
 import com.kotlindiscord.kord.extensions.utils.getJumpUrl
 import dev.kord.common.entity.ButtonStyle
+import dev.kord.core.behavior.UserBehavior
 import dev.kord.core.behavior.ban
 import dev.kord.core.behavior.channel.GuildMessageChannelBehavior
 import dev.kord.core.behavior.channel.createEmbed
 import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.behavior.edit
+import dev.kord.core.entity.Member
+import dev.kord.core.entity.Message
 import kotlinx.datetime.Clock
+import kotlinx.serialization.json.JsonNull.content
 import net.irisshaders.lilybot.utils.MESSAGE_LOGS
 import net.irisshaders.lilybot.utils.MODERATORS
 import kotlin.time.ExperimentalTime
@@ -44,106 +48,11 @@ class Report : Extension() {
                 respond {
                     content = "Message reported to staff"
                 }
-
-                actionLog.createMessage {
-                    content = "<@&${MODERATORS.value}>"
-                }
-
-                actionLog.createEmbed {
-                    color = DISCORD_RED
-                    title = "Message reported!"
-
-                    field {
-                        value = "**Messaged Content:** ${reportedMessage.content}"
-                        inline = true
-                    }
-                    field {
-                        value =
-                            "**Message Link:** ${reportedMessage.getJumpUrl()}"
-                        inline = false
-                    }
-                    field {
-                        value = "**Reported by:** ${user.asUser().tag}"
-                        inline = false
-                    }
-                    timestamp = Clock.System.now()
-                }.edit {
-                    components {
-                        ephemeralButton(row = 0) {
-                            label = "Delete the reported message"
-                            style = ButtonStyle.Danger
-
-                            action {
-                                reportedMessage.delete(reason = "Deleted via report.")
-                            }
-                        }
-
-                        ephemeralSelectMenu(row = 1) {
-                            option(
-                                label = "10-Minute Timeout",
-                                value = "10-timeout",
-                            ) {
-                                description = "Timeout the user for ten minutes."
-                            }
-                            option(
-                                label = "20-Minute Timeout",
-                                value = "20-timeout",
-                            ) {
-                                description = "Timeout the user for 20 minutes."
-                            }
-                            option(
-                                label = "30-Minute Timeout",
-                                value = "30-timeout",
-                            )
-                            {
-                                description = "Timeout the user for 30 minutes."
-                            }
-                            option(
-                                label = "Kick the user.",
-                                value = "kick-user",
-                            )
-                            {
-                                description = "Kick the user from the server."
-                            }
-                            option(
-                                label = "Softban the user.",
-                                value = "softban-user",
-                            )
-                            {
-                                description = "Softban the user and delete all their messages."
-                            }
-                            option(
-                                label = "Ban the user.",
-                                value = "ban-user",
-                            )
-                            {
-                                description = "Ban the user and delete their messages."
-                            }
-                            action {
-                                when (this.selected[0]) {
-                                    "10-timeout" -> println("10")
-                                    "20-timeout" -> println("20")
-                                    "30-timeout" -> println("30")
-                                    "kick-user" -> messageAuthor?.kick(reason = "Kicked via report")
-                                    "softban-user" -> {
-                                        messageAuthor?.ban {
-                                            this.reason = "Banned via report."
-                                            this.deleteMessagesDays = 1
-                                        }
-                                        reportedMessage.getGuild().unban(messageAuthor!!.id, reason = "Softban")
-                                    }
-                                    "ban-user" -> messageAuthor?.ban {
-                                        this.reason = "Banned via report"
-                                        this.deleteMessagesDays = 1
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                createReport(user, actionLog, messageAuthor, reportedMessage)
             }
 
         }
+        // Fuck this thing ngl
         ephemeralSlashCommand(::ManualReportArgs) {
             name = "manual-report"
             description = "Manually report a message"
@@ -154,30 +63,112 @@ class Report : Extension() {
                 respond {
                     content = "Message reported to staff"
                 }
-
-                actionLog.createMessage {
-                    content = "`<@&${MODERATORS.value}>`"
-                }
-
-                actionLog.createEmbed {
-                    color = DISCORD_RED
-                    title = "Message Manually Reported!"
-
-                    field {
-                        value = "**Message Link:** ${arguments.message}"
-                        inline = true
-                    }
-                    field {
-                        value = "**Reported by:** ${user.asUser().tag}"
-                        inline = false
-                    }
-                    timestamp = Clock.System.now()
-                }
+//                createReport(user, actionLog, messageAuthor, reportedMessage)
             }
         }
     }
 
     inner class ManualReportArgs : Arguments() {
         val message by string("message", "Link to the message to report")
+    }
+
+    private suspend fun createReport(user: UserBehavior, actionLog: GuildMessageChannelBehavior, messageAuthor: Member?, reportedMessage: Message) {
+        println("Test")
+        actionLog.createMessage {
+            content = "<@&${MODERATORS.value}>"
+        }
+
+        actionLog.createEmbed {
+            color = DISCORD_RED
+            title = "Message reported!"
+
+            field {
+                value = "**Messaged Content:** ${reportedMessage.content}"
+                inline = true
+            }
+            field {
+                value =
+                    "**Message Link:** ${reportedMessage.getJumpUrl()}"
+                inline = false
+            }
+            field {
+                value = "**Reported by:** ${user.asUser().tag}"
+                inline = false
+            }
+            timestamp = Clock.System.now()
+        }.edit {
+            components {
+                ephemeralButton(row = 0) {
+                    label = "Delete the reported message"
+                    style = ButtonStyle.Danger
+
+                    action {
+                        reportedMessage.delete(reason = "Deleted via report.")
+                    }
+                }
+
+                ephemeralSelectMenu(row = 1) {
+                    option(
+                        label = "10-Minute Timeout",
+                        value = "10-timeout",
+                    ) {
+                        description = "Timeout the user for ten minutes."
+                    }
+                    option(
+                        label = "20-Minute Timeout",
+                        value = "20-timeout",
+                    ) {
+                        description = "Timeout the user for 20 minutes."
+                    }
+                    option(
+                        label = "30-Minute Timeout",
+                        value = "30-timeout",
+                    )
+                    {
+                        description = "Timeout the user for 30 minutes."
+                    }
+                    option(
+                        label = "Kick the user.",
+                        value = "kick-user",
+                    )
+                    {
+                        description = "Kick the user from the server."
+                    }
+                    option(
+                        label = "Softban the user.",
+                        value = "softban-user",
+                    )
+                    {
+                        description = "Softban the user and delete all their messages."
+                    }
+                    option(
+                        label = "Ban the user.",
+                        value = "ban-user",
+                    )
+                    {
+                        description = "Ban the user and delete their messages."
+                    }
+                    action {
+                        when (this.selected[0]) {
+                            "10-timeout" -> println("10")
+                            "20-timeout" -> println("20")
+                            "30-timeout" -> println("30")
+                            "kick-user" -> messageAuthor?.kick(reason = "Kicked via report")
+                            "softban-user" -> {
+                                messageAuthor?.ban {
+                                    this.reason = "Banned via report."
+                                    this.deleteMessagesDays = 1
+                                }
+                                reportedMessage.getGuild().unban(messageAuthor!!.id, reason = "Softban")
+                            }
+                            "ban-user" -> messageAuthor?.ban {
+                                this.reason = "Banned via report"
+                                this.deleteMessagesDays = 1
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
