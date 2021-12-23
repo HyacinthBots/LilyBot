7300a210d9acfd6481329f1a4c4e6b71f0609836
maximumpower55 <62370320+maximumpower55@users.noreply.github.com>
2021-12-23 15:09:19 -0800

    Feature: log uploading

--- src/main/kotlin/net/irisshaders/lilybot/utils/ResponseHelper.kt
+++ src/main/kotlin/net/irisshaders/lilybot/utils/ResponseHelper.kt
@@ -3,13 +3,38 @@ package net.irisshaders.lilybot.utils
 import com.kotlindiscord.kord.extensions.DISCORD_BLACK
 import com.kotlindiscord.kord.extensions.DISCORD_RED
 import dev.kord.common.Color
+import dev.kord.core.behavior.channel.MessageChannelBehavior
 import dev.kord.core.behavior.channel.GuildMessageChannelBehavior
 import dev.kord.core.behavior.channel.createEmbed
 import dev.kord.core.entity.User
+import dev.kord.core.entity.Message
 import dev.kord.rest.builder.message.EmbedBuilder
 import kotlinx.datetime.Clock
 
 object ResponseHelper {
+    /**
+     * Creates an [EmbedBuilder] and populates it with the current timestamp, the given as the
+     * requester and the given [Color] as its color.
+     *
+     * @param embedTitle The title to set the embed to, or null to set no title.
+     * @param embedDescription The description of the embed. If null, the description will be empty.
+     * @param embedColor The color to set the embed to. If null, the embed will be coloured black.
+     * @author Maximumpower55
+     */
+    suspend fun responseEmbedInChannel(channel: MessageChannelBehavior, embedTitle: String?, embedDescription: String?, embedColor: Color?, requestedBy: User?): Message {
+        return channel.createEmbed {
+            title = embedTitle
+            if (embedDescription != null) description = embedDescription
+            color = embedColor
+            timestamp = Clock.System.now()
+            if (requestedBy != null) {
+                footer {
+                    text = requestedBy.tag
+                    icon = requestedBy.avatar?.url
+                }
+            }
+        }
+    }
 
     /**
      * This function generates a Failure embed for ease of use in other places
@@ -38,8 +63,8 @@ object ResponseHelper {
      * @param embedColor The color to set the embed to. If null, the embed will be coloured black.
      * @author NoComment1105
      */
-    suspend fun responseEmbedInActionLog(channel: GuildMessageChannelBehavior, embedTitle: String?, embedDescripton: String?, embedColor: Color?, requestedBy: User?) {
-        channel.createEmbed {
+    suspend fun responseEmbedInActionLog(channel: GuildMessageChannelBehavior, embedTitle: String?, embedDescripton: String?, embedColor: Color?, requestedBy: User?): Message {
+        return channel.createEmbed {
             title = embedTitle
             if (embedDescripton != null) description = embedDescripton
             color = embedColor
