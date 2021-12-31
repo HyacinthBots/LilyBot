953fc34b0fef2361281123a77a9364500a2b451e
NoComment1105 <NoComment1105@outlook.com>
2021-12-31 11:32:27 +0000

    Finally, fix member join logging I HOPE

--- src/main/kotlin/net/irisshaders/lilybot/LilyBot.kt
+++ src/main/kotlin/net/irisshaders/lilybot/LilyBot.kt
@@ -1,3 +1,5 @@
+@file:OptIn(PrivilegedIntent::class)
+
 package net.irisshaders.lilybot
 
 import com.github.jezza.Toml
@@ -6,27 +8,20 @@ import com.kotlindiscord.kord.extensions.ExtensibleBot
 import com.kotlindiscord.kord.extensions.modules.extra.phishing.DetectionAction
 import com.kotlindiscord.kord.extensions.modules.extra.phishing.extPhishing
 import dev.kord.common.entity.PresenceStatus
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
 import mu.KotlinLogging
-import net.irisshaders.lilybot.commands.Custom
-import net.irisshaders.lilybot.commands.Github
-import net.irisshaders.lilybot.commands.Moderation
-import net.irisshaders.lilybot.commands.Ping
-import net.irisshaders.lilybot.commands.Report
+import net.irisshaders.lilybot.commands.*
 import net.irisshaders.lilybot.database.DatabaseManager
 import net.irisshaders.lilybot.events.JoinLeaveEvent
 import net.irisshaders.lilybot.events.MessageEvents
 import net.irisshaders.lilybot.support.ThreadInviter
-import net.irisshaders.lilybot.utils.BOT_TOKEN
-import net.irisshaders.lilybot.utils.CONFIG_PATH
-import net.irisshaders.lilybot.utils.GITHUB_OAUTH
-import net.irisshaders.lilybot.utils.GUILD_ID
-import net.irisshaders.lilybot.utils.SENTRY_DSN
+import net.irisshaders.lilybot.utils.*
 import org.kohsuke.github.GitHub
 import org.kohsuke.github.GitHubBuilder
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import kotlin.Exception
 
 val configPath: Path = Paths.get(CONFIG_PATH)
 val config: TomlTable = Toml.from(Files.newInputStream(configPath))
@@ -38,6 +33,12 @@ suspend fun main() {
     val bot = ExtensibleBot(BOT_TOKEN) {
         applicationCommands {
             defaultGuild(GUILD_ID)
+        }
+        members {
+            fill(GUILD_ID)
+        }
+        intents {
+            +Intent.GuildMembers
         }
 
         chatCommands {
