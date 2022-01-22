b36aa2144920ac047df08ac6c46240dee28f6004
NoComment1105 <nocomment1105@outlook.com>
2022-01-22 05:43:05 +0000

    haha yes
    Signed-off-by: NoComment1105 <nocomment1105@outlook.com>

--- src/main/kotlin/net/irisshaders/lilybot/extensions/moderation/Report.kt
+++ src/main/kotlin/net/irisshaders/lilybot/extensions/moderation/Report.kt
@@ -283,7 +283,7 @@ class Report : Extension() {
 
     inner class ManualReportArgs : Arguments() {
         val message by string {
-            name = "message link"
+            name = "message-link"
             description = "Link to the message to report"
         }
     }
