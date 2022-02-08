8f8b360db1144735be4f437d22acc0b1816fa82a
NoComment1105 <nocomment1105@outlook.com>
2022-02-08 17:21:22 +0000

    Die die die die die die
    Signed-off-by: NoComment1105 <nocomment1105@outlook.com>

--- src/main/kotlin/net/irisshaders/lilybot/extensions/util/Config.kt
+++ src/main/kotlin/net/irisshaders/lilybot/extensions/util/Config.kt
@@ -5,8 +5,11 @@ package net.irisshaders.lilybot.extensions.util
 import com.kotlindiscord.kord.extensions.checks.hasPermission
 import com.kotlindiscord.kord.extensions.commands.Arguments
 import com.kotlindiscord.kord.extensions.commands.application.slash.ephemeralSubCommand
-import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalString
-import com.kotlindiscord.kord.extensions.commands.converters.impl.string
+import com.kotlindiscord.kord.extensions.commands.converters.impl.channel
+import com.kotlindiscord.kord.extensions.commands.converters.impl.guild
+import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalChannel
+import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalRole
+import com.kotlindiscord.kord.extensions.commands.converters.impl.role
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
@@ -36,13 +39,13 @@ class Config : Extension() {
 					newSuspendedTransaction {
 
 						DatabaseManager.Config.insertIgnore {
-							it[guildId] = arguments.guildId.parsed
-							it[moderatorsPing] = arguments.moderatorPing.parsed
-							it[supportTeam] = arguments.supportTeam.parsed
-							it[modActionLog] = arguments.modActionLog.parsed
-							it[messageLogs] = arguments.messageLogs.parsed
-							it[supportChanel] = arguments.supportChannel.parsed
-							it[joinChannel] = arguments.joinChannel.parsed
+							it[guildId] = arguments.guildId.id.toString()
+							it[moderatorsPing] = arguments.moderatorPing.id.toString()
+							it[supportTeam] = arguments.supportTeam?.id.toString()
+							it[modActionLog] = arguments.modActionLog.id.toString()
+							it[messageLogs] = arguments.messageLogs.id.toString()
+							it[supportChanel] = arguments.supportChannel?.id.toString()
+							it[joinChannel] = arguments.joinChannel.id.toString()
 						}
 					}
 
@@ -56,7 +59,7 @@ class Config : Extension() {
 					var guildConfig: String?
 					newSuspendedTransaction {
 						guildConfig = DatabaseManager.Config.select {
-							DatabaseManager.Config.guildId eq arguments.guidldId.parsed
+							DatabaseManager.Config.guildId eq arguments.guidldId.id.toString()
 						}.single()[DatabaseManager.Config.guildId]
 						println(guildConfig)
 					}
@@ -65,37 +68,37 @@ class Config : Extension() {
 		}
 	}
 	inner class Config : Arguments() {
-		val guildId = string {
+		val guildId by guild {
 			name = "guildId"
 			description = "The ID of your guild. Used for getting action log channels"
 		}
-		val moderatorPing = string {
+		val moderatorPing by role {
 			name = "moderatorRoleId"
 			description = "The ID of your Moderator role"
 		}
-		val modActionLog = string {
+		val modActionLog by channel {
 			name = "modActionLogId"
 			description = "The ID of your Mod Action Log channel"
 		}
-		val messageLogs = string {
+		val messageLogs by channel {
 			name = "messageLogsId"
 			description = "The ID of your Messsage Logs Channel"
 		}
-		val joinChannel = string {
+		val joinChannel by channel {
 			name = "joinChannelId"
 			description = "The ID of your Join Logs Channel"
 		}
-		val supportTeam = optionalString {
+		val supportTeam by optionalRole {
 			name = "supportTeamRoleId"
 			description = "The ID of your Support Team role"
 		}
-		val supportChannel = optionalString {
+		val supportChannel by optionalChannel {
 			name = "supportChannelId"
 			description = "The ID of your Support Channel "
 		}
 	}
 	inner class Clear : Arguments() {
-		val guidldId = string {
+		val guidldId by guild {
 			name = "guildId"
 			description = "The ID of the guild to clear the config for"
 		}
