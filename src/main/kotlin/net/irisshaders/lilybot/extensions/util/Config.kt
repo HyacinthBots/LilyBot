9f2e6821f5e23d41ac66084e12803bd2919ea72e
NoComment1105 <nocomment1105@outlook.com>
2022-02-10 16:34:03 +0000

    It is now better:tm:
    Signed-off-by: NoComment1105 <nocomment1105@outlook.com>

--- src/main/kotlin/net/irisshaders/lilybot/extensions/util/Config.kt
+++ src/main/kotlin/net/irisshaders/lilybot/extensions/util/Config.kt
@@ -14,8 +14,11 @@ import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.ephemeralSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
 import dev.kord.common.entity.Permission
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.behavior.channel.GuildMessageChannelBehavior
 import net.irisshaders.lilybot.database.DatabaseManager
 import net.irisshaders.lilybot.database.DatabaseManager.getConnection
+import net.irisshaders.lilybot.utils.ResponseHelper
 import org.jetbrains.exposed.sql.insertIgnore
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
@@ -39,21 +42,55 @@ class Config : Extension() {
 				name = "set"
 				description = "Set the config"
 				action {
+					var actionLogId: String? = null
+					var alreadySet = false
 
-					newSuspendedTransaction {
-
-						DatabaseManager.Config.insertIgnore {
-							it[guildId] = arguments.guildId.toString()
-							it[moderatorsPing] = arguments.moderatorPing.id.toString()
-							it[supportTeam] = arguments.supportTeam?.id.toString()
-							it[modActionLog] = arguments.modActionLog.id.toString()
-							it[messageLogs] = arguments.messageLogs.id.toString()
-							it[supportChanel] = arguments.supportChannel?.id.toString()
-							it[joinChannel] = arguments.joinChannel.id.toString()
+				    newSuspendedTransaction {
+						try {
+							DatabaseManager.Config.select {
+								DatabaseManager.Config.guildId eq arguments.guildId.id.toString()
+							}.single()[DatabaseManager.Config.guildId]
+							alreadySet = true
+							return@newSuspendedTransaction
+						} catch (e: NoSuchElementException) {
+							alreadySet = false
+							return@newSuspendedTransaction
+							// Swallow the error and return because we want to know this
 						}
 					}
 
-					respond { content = "Config Set for Guild ID: ${arguments.guildId}!" }
+					if (!alreadySet) {
+						newSuspendedTransaction {
+
+							DatabaseManager.Config.insertIgnore {
+								it[guildId] = arguments.guildId.id.toString()
+								it[moderatorsPing] = arguments.moderatorPing.id.toString()
+								it[supportTeam] = arguments.supportTeam?.id.toString()
+								it[modActionLog] = arguments.modActionLog.id.toString()
+								it[messageLogs] = arguments.messageLogs.id.toString()
+								it[supportChanel] = arguments.supportChannel?.id.toString()
+								it[joinChannel] = arguments.joinChannel.id.toString()
+							}
+
+							actionLogId = DatabaseManager.Config.select {
+								DatabaseManager.Config.guildId eq arguments.guildId.id.toString()
+							}.single()[DatabaseManager.Config.modActionLog]
+						}
+
+						respond { content = "Config Set for Guild ID: ${arguments.guildId.id}!" }
+
+						val actionLogChannel =
+							guild?.getChannel(Snowflake(actionLogId!!.toLong())) as GuildMessageChannelBehavior
+						ResponseHelper.responseEmbedInChannel(
+							actionLogChannel,
+							"Config Set!",
+							"An admin has set a config for this guild!",
+							null,
+							user.asUser()
+						)
+					} else {
+						respond { content = "Your config is already set, clear it first before updating!" }
+					}
 				}
 			}
 			ephemeralSubCommand(::Clear) {
@@ -61,25 +98,41 @@ class Config : Extension() {
 				description = "Clear the config!"
 				action {
 					var guildConfig: String? = null
+					var actionLogId: String? = null
 
 					newSuspendedTransaction {
 						guildConfig = DatabaseManager.Config.select {
-							DatabaseManager.Config.guildId eq arguments.guidldId.toString()
+							DatabaseManager.Config.guildId eq arguments.guildId.id.toString()
 						}.single()[DatabaseManager.Config.guildId]
+
+						actionLogId = DatabaseManager.Config.select {
+							DatabaseManager.Config.guildId eq arguments.guildId.id.toString()
+						}.single()[DatabaseManager.Config.modActionLog]
 					}
 
+					// Yes I did write raw SQL, even though we're using exposed.
+					// No I won't change it, exposed is clapped for removing stuff from the db
 					try {
 						val connection: Connection = getConnection()
 						val ps: PreparedStatement = connection.prepareStatement("DELETE FROM config WHERE guildId = ?")
 						ps.setString(1, guildConfig)
 						ps.executeUpdate()
 					} catch (e: SQLException) {
-						respond { content = "An error occured in updating the Database. Please report this!" }
+						respond { content = "An error occurred in updating the Database. Please report this!" }
 						e.printStackTrace()
 						return@action
 					}
 
 					respond { content = "Cleared config for Guild ID: $guildConfig" }
+
+					val actionLogChannel = guild?.getChannel(Snowflake(actionLogId!!.toLong())) as GuildMessageChannelBehavior
+					ResponseHelper.responseEmbedInChannel(
+						actionLogChannel,
+						"Config Cleared!",
+						"An admin has cleared the config for this guild!",
+						null,
+						user.asUser()
+					)
 				}
 			}
 		}
@@ -111,11 +164,11 @@ class Config : Extension() {
 		}
 		val supportChannel by optionalChannel {
 			name = "supportChannel"
-			description = "Your Support Channel "
+			description = "Your Support Channel"
 		}
 	}
 	inner class Clear : Arguments() {
-		val guidldId by guild {
+		val guildId by guild {
 			name = "guildId"
 			description = "The ID of your guild"
 		}
