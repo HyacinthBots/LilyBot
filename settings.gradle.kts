c8cda0e2b6fb7bcd0e9fdde108bd2a43dd2fed3d
NoComment1105 <nocomment1105@outlook.com>
2022-02-10 15:01:25 +0000

    Update buildscript
    Signed-off-by: NoComment1105 <nocomment1105@outlook.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,13 +2,10 @@ pluginManagement {
     plugins {
         // Update this in libs.version.toml when you change it here
         kotlin("jvm") version "1.6.10"
-        // Update this in libs.version.toml when you change it here
-        kotlin("plugin.serialization") version "1.6.10"
 
         id("com.github.johnrengelman.shadow") version "7.1.2"
     }
     repositories {
-        google()
         gradlePluginPortal()
     }
 }
