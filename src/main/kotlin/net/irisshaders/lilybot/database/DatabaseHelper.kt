d726b433790e46540d47b37a6a0ddff2fb768b4d
NoComment1105 <nocomment1105@outlook.com>
2022-02-12 18:28:44 +0000

    Document DatabaseHelper.kt
    Signed-off-by: NoComment1105 <nocomment1105@outlook.com>

--- src/main/kotlin/net/irisshaders/lilybot/database/DatabaseHelper.kt
+++ src/main/kotlin/net/irisshaders/lilybot/database/DatabaseHelper.kt
@@ -7,6 +7,15 @@ import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransacti
 
 object DatabaseHelper {
 
+	/**
+	 * Using the provided [guildId] and [column] a [value] or [error] will be returned from the
+	 * config database
+	 *
+	 * @param guildId The ID of the guild the command was run in
+	 * @param column The column you want the information form
+	 * @return a [NoSuchElementException] or the result from the Database
+	 * @author NoComment1105
+	 */
 	@Suppress("DuplicatedCode")
 	@JvmName("selectInConfig!!")
 	suspend fun selectInConfig(guildId: Snowflake, column: Column<String>): String? {
@@ -24,6 +33,15 @@ object DatabaseHelper {
 		return if (error == null) value else error
 	}
 
+	/**
+	 * Using the provided [guildId] and nullable [column] a [value] or [error] will be returned from the
+	 * config database
+	 *
+	 * @param guildId The ID of the guild the command was run in
+	 * @param column The column you want the information from. Nullable
+	 * @return a [NoSuchElementException] or the result from the Database
+	 * @author NoComment1105
+	 */
 	@Suppress("DuplicatedCode")
 	@JvmName("selectInConfig?")
 	suspend fun selectInConfig(guildId: Snowflake, column: Column<String?>): String? {
@@ -41,6 +59,15 @@ object DatabaseHelper {
 		return if (error == null) value else error
 	}
 
+	/**
+	 * Using the provided [componentId] and nullable [column] a [value] or [error] will be returned from the
+	 * config database
+	 *
+	 * @param componentId The ID of the component you're looking for.
+	 * @param column The column you want the information from.
+	 * @return a [NoSuchElementException] or the result from the Database.
+	 * @author NoComment1105
+	 */
 	suspend fun selectInComponents(componentId: String, column: Column<String>): String? {
 		var value: String? = null
 		var error: String? = null
