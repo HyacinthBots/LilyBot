758278afc4693a847cc435ea0c75d6858d7040c1
tempest15 <tempest.15.22.15@gmail.com>
2022-03-11 14:30:22 -0500

    new: add javadocs for DatabaseHelper.kt

--- src/main/kotlin/net/irisshaders/lilybot/utils/DatabaseHelper.kt
+++ src/main/kotlin/net/irisshaders/lilybot/utils/DatabaseHelper.kt
@@ -7,18 +7,14 @@ import org.litote.kmongo.eq
 object DatabaseHelper {
 
 	/**
-	 * Using the provided [guildId] and [column] a value or error will be returned from the
-	 * config database
+	 * Using the provided [inputGuildId] and [inputColumn] a value or null will be returned from the database
 	 *
 	 * @param inputGuildId The ID of the guild the command was run in
 	 * @param inputColumn The config value associated with that guild that you want
-	 * @return a [NoSuchElementException] or the result from the Database
+	 * @return null or the result from the database
 	 * @author NoComment1105
 	 * @author tempest15
 	 */
-
-	// todo write javadocs for all of these
-
 	suspend fun selectInConfig(inputGuildId: String, inputColumn: String): String? {
 		var selectedConfig: ConfigData?
 
@@ -39,6 +35,13 @@ object DatabaseHelper {
 		}
 	}
 
+	/**
+	 * A new config based on the given [newConfig] will be set for the provided [inputGuildId]
+	 *
+	 * @param inputGuildId The ID of the guild the command was run in
+	 * @param newConfig The new config values you want to set
+	 * @author tempest15
+	 */
 	suspend fun setConfig(inputGuildId: String, newConfig: ConfigData) {
 		runBlocking {
 			val collection = database.getCollection<ConfigData>()
@@ -47,6 +50,12 @@ object DatabaseHelper {
 		}
 	}
 
+	/**
+	 * Clears the config for the provided [inputGuildId]
+	 *
+	 * @param inputGuildId The ID of the guild the command was run in
+	 * @author tempest15
+	 */
 	suspend fun clearConfig(inputGuildId: String) {
 		runBlocking {
 			val collection = database.getCollection<ConfigData>()
@@ -55,6 +64,14 @@ object DatabaseHelper {
 
 	}
 
+	/**
+	 * Gets the number of points the provided [inputUserId] has in the provided [inputGuildId] from the database
+	 *
+	 * @return null or the result from the database
+	 * @param inputUserId The ID of the user to get the point value for
+	 * @param inputGuildId The ID of the guild the command was run in
+	 * @author tempest15
+	 */
 	suspend fun selectInWarn(inputUserId: String, inputGuildId: String): Int? {
 		var selectedUserInGuild: WarnData?
 
@@ -70,6 +87,13 @@ object DatabaseHelper {
 		}
 	}
 
+	/**
+	 * Updates the number of points the provided [inputUserId] has in the provided [inputGuildId] in the database
+	 *
+	 * @param inputUserId The ID of the user to get the point value for
+	 * @param inputGuildId The ID of the guild the command was run in
+	 * @author tempest15
+	 */
 	suspend fun putInWarn(inputUserId: String, inputGuildId: String, inputPointValue: Int) {
 		var selectedUserInGuild: WarnData?
 
