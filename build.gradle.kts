2e69e6e70da5e230208df714fb9ad1936e94d56c
tempest15 <tempest.15.22.15@gmail.com>
2022-04-04 12:07:13 -0400

    remove database migration dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,13 +62,6 @@ dependencies {
 
     // KMongo
     implementation(libs.kmongo)
-
-    // todo remove this
-    implementation("org.jetbrains.exposed:exposed-core:0.37.3")
-    implementation("org.jetbrains.exposed:exposed-dao:0.37.3")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.37.3")
-    implementation("com.zaxxer:HikariCP:5.0.1")
-    implementation("org.xerial:sqlite-jdbc:3.36.0.3")
 }
 
 application {
