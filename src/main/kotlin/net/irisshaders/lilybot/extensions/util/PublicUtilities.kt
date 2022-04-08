47ce4025710563327cc00b91efc3558b09c615f9
NoComment1105 <nocomment1105@outlook.com>
2022-04-08 08:06:15 +0100

    Create a nickname clear command
    Signed-off-by: NoComment1105 <nocomment1105@outlook.com>

--- src/main/kotlin/net/irisshaders/lilybot/extensions/util/PublicUtilities.kt
+++ src/main/kotlin/net/irisshaders/lilybot/extensions/util/PublicUtilities.kt
@@ -4,6 +4,7 @@ import com.kotlindiscord.kord.extensions.DISCORD_GREEN
 import com.kotlindiscord.kord.extensions.DISCORD_RED
 import com.kotlindiscord.kord.extensions.DISCORD_YELLOW
 import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.application.slash.ephemeralSubCommand
 import com.kotlindiscord.kord.extensions.commands.converters.impl.string
 import com.kotlindiscord.kord.extensions.components.components
 import com.kotlindiscord.kord.extensions.components.ephemeralButton
@@ -77,114 +78,118 @@ class PublicUtilities : Extension() {
 
 		// TODO: Permission stuff? Accept returns a permission error
 		// FIXME: This code is cursed, clean it up a little
-		ephemeralSlashCommand(::NickRequestArgs) {
-			name = "request-nickname"
-			description = "This command allows you to request a new nickname for the server."
+		ephemeralSlashCommand {
+			name = "nickname"
+			description = "Nickname related commands"
 
-			action {
-				val actionLogId = getConfigPublicResponse("modActionLog") ?: return@action
-				val actionLog = guild?.getChannel(actionLogId) as GuildMessageChannelBehavior
-				val requester = user.asUser()
-				var responseEmbed: Message? = null
+			ephemeralSubCommand(::NickRequestArgs) {
+				name = "request"
+				description = "This command allows you to request a new nickname for the server."
+				action {
+					val actionLogId = getConfigPublicResponse("modActionLog") ?: return@action
+					val actionLog = guild?.getChannel(actionLogId) as GuildMessageChannelBehavior
+					val requester = user.asUser()
+					var responseEmbed: Message? = null
 
-				respond { content = "Nickname request sent!" }
+					respond { content = "Nickname request sent!" }
 
-				responseEmbed = actionLog.createEmbed {
-					color = DISCORD_YELLOW
-					title = "Nickname Request"
-					timestamp = Clock.System.now()
+					responseEmbed = actionLog.createEmbed {
+						color = DISCORD_YELLOW
+						title = "Nickname Request"
+						timestamp = Clock.System.now()
 
-					field {
-						name = "User:"
-						value = "${user.mention}\n${user.asUser().tag}\n${user.id}"
-						inline = false
-					}
-					field {
-						name = "Requested Nickname:"
-						value = "${user.asMember(guild!!.id).nickname} -> ${arguments.newNick}"
-						inline = false
-					}
-				}.edit {
-					components {
-						ephemeralButton(row = 0) {
-							label = "Accept Nickname"
-							style = ButtonStyle.Success
-
-							action {
-								user.asMember(guild!!.id).edit { nickname = arguments.newNick }
-
-								userDMEmbed(
-									user.asUser(),
-									"Nickname Change accepted",
-									null,
-									DISCORD_GREEN
-								)
-								clearComponents(responseEmbed)
-
-								actionLog.createMessage("Nickname Accepted")
-							}
+						field {
+							name = "User:"
+							value = "${user.mention}\n${user.asUser().tag}\n${user.id}"
+							inline = false
 						}
-						ephemeralButton(row = 0) {
-							label = "Deny Nickname"
-							style = ButtonStyle.Danger
+						field {
+							name = "Requested Nickname:"
+							value = "${user.asMember(guild!!.id).nickname} -> ${arguments.newNick}"
+							inline = false
+						}
+					}.edit {
+						components {
+							ephemeralButton(row = 0) {
+								label = "Accept Nickname"
+								style = ButtonStyle.Success
 
-							var reason: String? = null
-							var reasonEmbed: Message? = null
+								action {
+									user.asMember(guild!!.id).edit { nickname = arguments.newNick }
 
-							action {
-								reasonEmbed = actionLog.createEmbed {
-									description = "Why are you denying this username?"
-								}.edit {
-									components {
-										ephemeralSelectMenu(row = 1) {
-											placeholder = "Select reason"
-											option(
-												label = "Inappropriate Nickname",
-												value = "inappropriate"
-											) {
-												description = "This nickname is deemed inappropriate"
-											}
-											option(
-												label = "Impersonates Others",
-												value = "impersonation"
-											) {
-												description = "This nickname impersonates someone"
-											}
-											option(
-												label = "Hoisting",
-												value = "hoisting"
-											) {
-												description = "This nickname deliberately hoists the user"
-											}
+									userDMEmbed(
+										user.asUser(),
+										"Nickname Change accepted",
+										null,
+										DISCORD_GREEN
+									)
+									clearComponents(responseEmbed)
 
-											action {
-												when (this.selected[0]) {
-													"inappropriate" -> {
-														reason = "This nickname is inappropriate for this server."
-													}
-													"impersonation" -> {
-														reason = "This nickname impersonates another user."
-													}
-													"hoisting" -> {
-														reason =
-															"This nickname deliberately hoists you up the user ladder. " +
-																	"Which is not allowed."
-													}
+									actionLog.createMessage("Nickname Accepted")
+								}
+							}
+							ephemeralButton(row = 0) {
+								label = "Deny Nickname"
+								style = ButtonStyle.Danger
+
+								var reason: String? = null
+								var reasonEmbed: Message? = null
+
+								action {
+									reasonEmbed = actionLog.createEmbed {
+										description = "Why are you denying this username?"
+									}.edit {
+										components {
+											ephemeralSelectMenu(row = 1) {
+												placeholder = "Select reason"
+												option(
+													label = "Inappropriate Nickname",
+													value = "inappropriate"
+												) {
+													description = "This nickname is deemed inappropriate"
+												}
+												option(
+													label = "Impersonates Others",
+													value = "impersonation"
+												) {
+													description = "This nickname impersonates someone"
+												}
+												option(
+													label = "Hoisting",
+													value = "hoisting"
+												) {
+													description = "This nickname deliberately hoists the user"
 												}
 
-												userDMEmbed(
-													requester.asUser(),
-													"Nickname Change Denied",
-													"Staff have review your nickname request and decided to reject it " +
-															"for reason:\n\n${reason} Please choose another nickname",
-													DISCORD_RED
-												)
+												action {
+													when (this.selected[0]) {
+														"inappropriate" -> {
+															reason = "This nickname is inappropriate for this server."
+														}
+														"impersonation" -> {
+															reason = "This nickname impersonates another user."
+														}
+														"hoisting" -> {
+															reason =
+																"This nickname deliberately hoists you up the user ladder. " +
+																		"Which is not allowed."
+														}
+													}
 
-												clearComponents(responseEmbed)
-												clearComponents(reasonEmbed)
-												reasonEmbed!!.delete()
+													userDMEmbed(
+														requester.asUser(),
+														"Nickname Change Denied",
+														"Staff have review your nickname request and decided to reject it " +
+																"for reason:\n\n${reason} Please choose another nickname",
+														DISCORD_RED
+													)
 
-												actionLog.createMessage("Nickname Denied. Reason:\n$reason")
+													clearComponents(responseEmbed)
+													clearComponents(reasonEmbed)
+													reasonEmbed!!.delete()
+
+													actionLog.createMessage("Nickname Denied. Reason:\n$reason")
+												}
 											}
 										}
 									}
@@ -192,6 +197,34 @@ class PublicUtilities : Extension() {
 							}
 						}
 					}
+				}
+			}
+
+			ephemeralSubCommand {
+				name = "clear"
+				description = "Clear your current nickname"
+
+				action {
+					val actionLogId = getConfigPublicResponse("modActionLog") ?: return@action
+					val actionLog = guild?.getChannel(actionLogId) as GuildMessageChannelBehavior
+
+					actionLog.createEmbed {
+						title = "Nickname cleared"
+						timestamp = Clock.System.now()
+
+						field {
+							name = "User:"
+							value = "${user.mention}\n${user.asUser().tag}\n${user.id}"
+							inline = false
+						}
+						field {
+							name = "Nickname changed from"
+							value = "${user.asMember(guild!!.id).nickname} -> null"
+							inline = false
+						}
+					}
+
+					user.asMember(guild!!.id).edit { nickname = null }
 				}
 			}
 		}
