e94231bd9ea957707539636c4e81e8994bbdc8f0
NoComment1105 <nocomment1105@outlook.com>
2022-04-13 14:39:40 +0100

    Logging :>
    Signed-off-by: NoComment1105 <nocomment1105@outlook.com>

--- src/main/kotlin/net/irisshaders/lilybot/extensions/util/Tags.kt
+++ src/main/kotlin/net/irisshaders/lilybot/extensions/util/Tags.kt
@@ -1,6 +1,8 @@
 package net.irisshaders.lilybot.extensions.util
 
 import com.kotlindiscord.kord.extensions.DISCORD_BLURPLE
+import com.kotlindiscord.kord.extensions.DISCORD_GREEN
+import com.kotlindiscord.kord.extensions.DISCORD_RED
 import com.kotlindiscord.kord.extensions.checks.anyGuild
 import com.kotlindiscord.kord.extensions.checks.hasPermission
 import com.kotlindiscord.kord.extensions.commands.Arguments
@@ -12,9 +14,12 @@ import com.kotlindiscord.kord.extensions.types.respond
 import com.kotlindiscord.kord.extensions.types.respondEphemeral
 import com.kotlindiscord.kord.extensions.utils.suggestStringMap
 import dev.kord.common.entity.Permission
+import dev.kord.core.behavior.channel.GuildMessageChannelBehavior
 import dev.kord.rest.builder.message.create.embed
 import kotlinx.datetime.Clock
 import net.irisshaders.lilybot.utils.DatabaseHelper
+import net.irisshaders.lilybot.utils.getConfigPrivateResponse
+import net.irisshaders.lilybot.utils.responseEmbedInChannel
 
 class Tags : Extension() {
 	override val name = "tags"
@@ -55,11 +60,22 @@ class Tags : Extension() {
 			check { hasPermission(Permission.ModerateMembers) }
 
 			action {
-				DatabaseHelper.setTag(guild!!.id, arguments.name, arguments.tagTitle, arguments.tagValue)
+				DatabaseHelper.setTag(guild!!.id, arguments.tagName, arguments.tagTitle, arguments.tagValue)
 
 				respond {
-					content = "Tag: `${arguments.name}` created"
+					content = "Tag: `${arguments.tagName}` created"
 				}
+
+				val actionLogId = getConfigPrivateResponse("actionLog") ?: return@action
+				val actionLog = guild!!.getChannel(actionLogId) as GuildMessageChannelBehavior
+
+				responseEmbedInChannel(
+					actionLog,
+					"Tag created!",
+					"The tag ${arguments.tagName} was created by ${user.asUser().mention}",
+					DISCORD_GREEN,
+					user.asUser()
+				)
 			}
 		}
 
@@ -83,6 +99,18 @@ class Tags : Extension() {
 				respond {
 					content = "Tag: `${arguments.tagName}` deleted"
 				}
+
+
+				val actionLogId = getConfigPrivateResponse("actionLog") ?: return@action
+				val actionLog = guild!!.getChannel(actionLogId) as GuildMessageChannelBehavior
+
+				responseEmbedInChannel(
+					actionLog,
+					"Tag deleted!",
+					"The tag ${arguments.tagName} was deleted by ${user.asUser().mention}",
+					DISCORD_RED,
+					user.asUser()
+				)
 			}
 		}
 	}
@@ -106,7 +134,7 @@ class Tags : Extension() {
 	}
 
 	inner class CreateTagArgs : Arguments() {
-		val name by string {
+		val tagName by string {
 			name = "tagName"
 			description = "The name of the tag you're making"
 		}
