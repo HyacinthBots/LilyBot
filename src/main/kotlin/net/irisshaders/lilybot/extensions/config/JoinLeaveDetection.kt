4c4719aec5670681b7cb0d2bb5a97345760ea596
NoComment1105 <nocomment1105@outlook.com>
2022-05-05 07:01:21 +0100

    Fix some of the action stuff

--- src/main/kotlin/net/irisshaders/lilybot/extensions/config/JoinLeaveDetection.kt
+++ src/main/kotlin/net/irisshaders/lilybot/extensions/config/JoinLeaveDetection.kt
@@ -15,11 +15,6 @@ class JoinLeaveDetection : Extension() {
 			action {
 				if (event.user.id != kord.selfId) return@action // We only care if Lily is leaving, so ignore others
 
-				val guildLeaveData = DatabaseHelper.getLeaveTime(event.guildId) ?: return@action
-
-				// This should never happen unless Lily is re-invited to a guild
-				if (guildLeaveData.guildLeaveTime != null) return@action
-
 				DatabaseHelper.setLeaveTime(event.guildId, Clock.System.now())
 			}
 		}
@@ -27,11 +22,6 @@ class JoinLeaveDetection : Extension() {
 		event<MemberJoinEvent> {
 			action {
 				if (event.member.id != kord.selfId) return@action // We only care if Lily is joining, so ignore others
-
-				val guildLeaveData = DatabaseHelper.getLeaveTime(event.guildId) ?: return@action
-
-				// If this is the case, it is first time join. Return to avoid removing things that don't exist
-				if (guildLeaveData.guildLeaveTime == null) return@action
 
 				DatabaseHelper.setLeaveTime(event.guildId, null)
 			}
