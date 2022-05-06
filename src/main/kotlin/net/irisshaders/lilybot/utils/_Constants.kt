94f799b3353d8c3178543a6a99dee9d3d6998e57
NoComment1105 <nocomment1105@outlook.com>
2022-05-06 19:50:18 +0100

    This is angering

--- src/main/kotlin/net/irisshaders/lilybot/utils/_Constants.kt
+++ src/main/kotlin/net/irisshaders/lilybot/utils/_Constants.kt
@@ -4,9 +4,17 @@ import com.kotlindiscord.kord.extensions.utils.env
 import com.kotlindiscord.kord.extensions.utils.envOrNull
 import dev.kord.common.entity.Snowflake
 
-// Allows for easier imports of the secret magic needed
+/** The Bot Token. */
 val BOT_TOKEN = env("TOKEN")
+
+/** The ID of the guild used for testing features. */
 val TEST_GUILD_ID = Snowflake(env("TEST_GUILD_ID"))
+
+/** The channel in the [TEST_GUILD_ID] where online notifications will be posted. */
 val ONLINE_STATUS_CHANNEL = Snowflake(env("ONLINE_STATUS_CHANNEL"))
+
+/** The string for connection to the database, defaults to local host. */
 val MONGO_URI = envOrNull("MONGO_URI") ?: "mongodb://localhost:27017"
+
+/** Sentry connection DSN. If null, Sentry won't be used. */
 val SENTRY_DSN = envOrNull("SENTRY_DSN")
