00cd28f386c4ff676572a316263459cab4e5488d
Max Henkel <max@maxhenkel.de>
2022-05-21 08:01:50 -0700

    Fix errors when deleting threads (#145)
    * Fix deleting threads causes errors
    
    * Rename removeThread to deleteThread and threadId to inputThreadId

--- src/main/kotlin/net/irisshaders/lilybot/utils/DatabaseHelper.kt
+++ src/main/kotlin/net/irisshaders/lilybot/utils/DatabaseHelper.kt
@@ -275,6 +275,16 @@ object DatabaseHelper {
 	}
 
 	/**
+	 * This function deletes the ownership data stored in the database for the given [inputThreadId].
+	 *
+	 * @param inputThreadId The ID of the thread to delete
+	 */
+	suspend fun deleteThread(inputThreadId: Snowflake) {
+		val collection = database.getCollection<ThreadData>()
+		collection.deleteOne(ThreadData::threadId eq inputThreadId)
+	}
+
+	/**
 	 * This function deletes the ownership data stored in the database for any thread older than a week.
 	 *
 	 * @author tempest15
