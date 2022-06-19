60f41883f5e4b0c3b813ad61adc9df495c9852cb
NoComment1105 <nocomment1105@outlook.com>
2022-06-19 10:57:14 +0100

    Bump to 3.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "net.irisshaders.lilybot"
-version = "3.4.1"
+version = "3.4.2"
 
 val javaVersion = 17
 val kotlinVersion = "${libs.versions.kotlin.get().split(".")[0]}.${libs.versions.kotlin.get().split(".")[1]}"
