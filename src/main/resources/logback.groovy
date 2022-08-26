e5a5956668d2c604b606b97efe721b1f81211000
NoComment1105 <nocomment1105@outlook.com>
2022-08-26 09:03:22 +0100

    Remove necessary parts of logback.groovy

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,24 +1,14 @@
 import ch.qos.logback.core.joran.spi.ConsoleTarget
 
-def environment = System.getenv().getOrDefault("ENVIRONMENT", "production")
-
-def defaultLevel = INFO
-def defaultTarget = ConsoleTarget.SystemErr
-
-if (environment == "dev") {
-    defaultLevel = DEBUG
-    defaultTarget = ConsoleTarget.SystemOut
-
-    // Silence warning about missing native PRNG
-    logger("io.ktor.util.random", ERROR)
-}
+// Silence warning about missing native PRNG
+logger("io.ktor.util.random", ERROR)
 
 appender("CONSOLE", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%boldMagenta(%d{dd-MM-yyyy HH:mm:ss}) %gray(|) %boldCyan(%-30.-30thread) %gray(|) %highlight(%-5level) %gray(|) %boldGreen(%-40.40logger{40}) %gray(|) %msg%n"
     }
 
-    target = defaultTarget
+    target = ConsoleTarget.SystemErr
 }
 
-root(defaultLevel, ["CONSOLE"])
+root(INFO, ["CONSOLE"])
