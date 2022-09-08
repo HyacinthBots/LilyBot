c3fcc28504c89166e65a048dc4f8b098b1aca013
trainb0y <trainb0ydev@gmail.com>
2022-09-07 18:49:33 -0600

    Update delete logging to match edit logging

--- src/main/kotlin/org/hyacinthbots/lilybot/extensions/events/MessageDelete.kt
+++ src/main/kotlin/org/hyacinthbots/lilybot/extensions/events/MessageDelete.kt
@@ -4,14 +4,13 @@ import com.kotlindiscord.kord.extensions.DISCORD_PINK
 import com.kotlindiscord.kord.extensions.checks.anyGuild
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.event
+import com.kotlindiscord.kord.extensions.modules.extra.pluralkit.api.PKMessage
 import com.kotlindiscord.kord.extensions.modules.extra.pluralkit.events.ProxiedMessageDeleteEvent
 import com.kotlindiscord.kord.extensions.modules.extra.pluralkit.events.UnProxiedMessageDeleteEvent
 import dev.kord.core.behavior.channel.asChannelOf
 import dev.kord.core.behavior.channel.createMessage
-import dev.kord.core.behavior.getChannelOf
-import dev.kord.core.entity.Attachment
+import dev.kord.core.entity.Message
 import dev.kord.core.entity.channel.GuildMessageChannel
-import dev.kord.rest.builder.message.EmbedBuilder
 import dev.kord.rest.builder.message.create.embed
 import kotlinx.datetime.Clock
 import org.hyacinthbots.lilybot.database.collections.LoggingConfigCollection
@@ -19,6 +18,8 @@ import org.hyacinthbots.lilybot.extensions.config.ConfigOptions
 import org.hyacinthbots.lilybot.extensions.config.ConfigType
 import org.hyacinthbots.lilybot.utils.configPresent
 import org.hyacinthbots.lilybot.utils.getLoggingChannelWithPerms
+import org.hyacinthbots.lilybot.utils.ifNullOrEmpty
+import org.hyacinthbots.lilybot.utils.trimmedContents
 
 /**
  * The class for logging deletion of messages to the guild message log.
@@ -30,9 +31,9 @@ class MessageDelete : Extension() {
 
 	override suspend fun setup() {
 		/**
-		 * Logs deleted messages in a guild to the message log channel designated in the config for that guild
+		 * Logs proxied deleted messages in a guild to the message log channel designated in the config for that guild
 		 * @author NoComment1105
-		 * @since 2.0
+		 * @see messageDelete
 		 */
 		event<ProxiedMessageDeleteEvent> {
 			check {
@@ -44,58 +45,15 @@ class MessageDelete : Extension() {
 			}
 
 			action {
-				val config = LoggingConfigCollection().getConfig(event.getGuild().id) ?: return@action
-				val messageLog =
-					getLoggingChannelWithPerms(event.getGuild(), config.messageChannel!!, ConfigType.LOGGING)
-						?: return@action
-
-				val originalMessage = event.message
-				val proxiedMessage = event.pkMessage
-				val messageContent = if (originalMessage?.asMessageOrNull() != null) {
-					if (originalMessage.asMessageOrNull().content.length > 1024) {
-						originalMessage.asMessageOrNull().content.substring(0, 1024) + "..."
-					} else {
-						originalMessage.asMessageOrNull().content
-					}
-				} else {
-					null
-				}
-				val messageLocation = event.pkMessage.channel
-				val attachments = event.message?.attachments
-				val images: MutableSet<Attachment> = mutableSetOf()
-				attachments?.forEach { if (it.isImage) images += it }
-
-				messageLog.createMessage {
-					embed {
-						color = DISCORD_PINK
-						author {
-							name = "Message deleted"
-							icon = proxiedMessage.member.avatarUrl
-						}
-						description =
-							"Location: ${event.getGuild().getChannelOf<GuildMessageChannel>(messageLocation).mention}" +
-									"(${event.getGuild().getChannelOf<GuildMessageChannel>(messageLocation).name})"
-						timestamp = Clock.System.now()
-
-						fields(messageContent, attachments)
-
-						field {
-							name = "Message Author:"
-							value = "System Member: ${proxiedMessage.member.name}\n" +
-									"Account: ${event.getGuild().getMember(proxiedMessage.sender).tag} " +
-									event.getGuild().getMember(proxiedMessage.sender).mention
-							inline = true
-						}
-
-						field {
-							name = "Author ID:"
-							value = proxiedMessage.sender.toString()
-						}
-					}
-				}
+				messageDelete(event.getMessage(), event.pkMessage)
 			}
 		}
 
+		/**
+		 * Logs unproxied deleted messages in a guild to the message log channel designated in the config for that guild
+		 * @author NoComment1105
+		 * @see messageDelete
+		 */
 		event<UnProxiedMessageDeleteEvent> {
 			check {
 				anyGuild()
@@ -107,94 +65,78 @@ class MessageDelete : Extension() {
 			}
 
 			action {
-				val config = LoggingConfigCollection().getConfig(event.getGuild().id) ?: return@action
+				messageDelete(event.getMessage(), null)
+			}
+		}
+	}
 
-				val message = event.message
+	/**
+	 * If message logging is enabled, sends an embed describing the message deletion to the guild's message log channel
+	 *
+	 * @param message The deleted message
+	 * @param proxiedMessage Extra data for PluralKit proxied messages
+	 * @author trainb0y
+	 */
+	private suspend fun messageDelete(message: Message, proxiedMessage: PKMessage?) {
+		val guild = message.getGuild()
+		val config = LoggingConfigCollection().getConfig(guild.id) ?: return
+		val messageLog =
+			getLoggingChannelWithPerms(message.getGuild(), config.messageChannel!!, ConfigType.LOGGING)
+				?: return
 
-				val messageLog =
-					getLoggingChannelWithPerms(event.getGuild(), config.messageChannel!!, ConfigType.LOGGING)
-						?: return@action
+		messageLog.createMessage {
+			embed {
+				color = DISCORD_PINK
+				author {
+					name = "Message deleted"
+					icon = proxiedMessage?.member?.avatarUrl ?: message.author?.avatar?.url
+				}
+				description =
+					"Location: ${message.channel.mention}" +
+							"(${message.channel.asChannelOf<GuildMessageChannel>().name})"
+				timestamp = Clock.System.now()
 
-				val messageContent = if (message?.asMessageOrNull() != null) {
-					if (message.asMessageOrNull().content.length > 1024) {
-						message.asMessageOrNull().content.substring(0, 1024) + "..."
-					} else {
-						message.asMessageOrNull().content
+				field {
+					name = "Message contents"
+					value = message.trimmedContents().ifNullOrEmpty { "Failed to retrieve previous message contents" }
+					inline = false
+				}
+
+				if (message.attachments.isNotEmpty()) {
+					field {
+						name = "Attachments"
+						value = message.attachments.map { it.url }.joinToString { "\n" }
+						inline = false
+					}
+				}
+
+				if (proxiedMessage != null) {
+					field {
+						name = "Message Author:"
+						value = "System Member: ${proxiedMessage.member.name}\n" +
+								"Account: ${guild.getMember(proxiedMessage.sender).tag} " +
+								guild.getMember(proxiedMessage.sender).mention
+						inline = true
+					}
+
+					field {
+						name = "Author ID:"
+						value = proxiedMessage.sender.toString()
 					}
 				} else {
-					null
-				}
+					field {
+						name = "Message Author:"
+						value =
+							"${message.author?.tag ?: "Failed to get author of message"} ${message.author?.mention ?: ""}"
+						inline = true
+					}
 
-				message ?: return@action
-
-				val messageLocation = event.channel.asChannelOf<GuildMessageChannel>()
-				val attachments = event.message?.attachments
-				val images: MutableSet<Attachment> = mutableSetOf()
-				attachments?.forEach { if (it.isImage) images += it }
-
-				messageLog.createMessage {
-					embed {
-						color = DISCORD_PINK
-						author {
-							name = "Message deleted"
-							icon = message.author?.avatar?.url
-						}
-						description =
-							"Location: ${messageLocation.mention}" +
-									"(${messageLocation.name})"
-						timestamp = Clock.System.now()
-
-						fields(messageContent, attachments)
-
-						field {
-							name = "Message Author:"
-							value =
-								"${message.author?.tag ?: "Failed to get author of message"} ${message.author?.mention ?: ""}"
-							inline = true
-						}
-
-						field {
-							name = "Author ID:"
-							value = message.author?.id.toString()
-						}
+					field {
+						name = "Author ID:"
+						value = message.author?.id.toString()
 					}
 				}
 			}
-		}
-	}
-}
-
-/**
- * Adds the common fields to a deleted message embed.
- *
- * @param messageContent The content of the message.
- * @param attachments The attachments of the message.
- *
- * @author NoComment1105
- * @since 3.6.0
- */
-private fun EmbedBuilder.fields(messageContent: String?, attachments: Set<Attachment>?) {
-	field {
-		name = "Message contents"
-		value =
-			if (messageContent.isNullOrEmpty()) {
-				"Failed to retrieve message contents"
-			} else {
-				messageContent
-			}
-		inline = false
-	}
-	if (!attachments.isNullOrEmpty()) {
-		val attachmentUrls = StringBuilder()
-		attachments.forEach {
-			attachmentUrls.append(
-				it.url + "\n"
-			)
-		}
-		field {
-			name = "Attachments"
-			value = attachmentUrls.trim().toString()
-			inline = false
 		}
 	}
 }
