826043f1c4069a84101946814c9eb2c7c4d60a94
NoComment1105 <nocomment1105@outlook.com>
2022-10-03 01:06:30 +0100

    Update to kotlin 1.7.20 and do dependency stuff

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     plugins {
         // Update this in libs.version.toml when you change it here
-        kotlin("jvm") version "1.7.10"
-        kotlin("plugin.serialization") version "1.7.10"
+        kotlin("jvm") version "1.7.20"
+        kotlin("plugin.serialization") version "1.7.20"
 
         id("com.github.johnrengelman.shadow") version "7.1.2"
 
@@ -16,8 +16,6 @@ pluginManagement {
 }
 
 rootProject.name = "LilyBot"
-
-enableFeaturePreview("VERSION_CATALOGS")
 
 dependencyResolutionManagement {
     versionCatalogs {
