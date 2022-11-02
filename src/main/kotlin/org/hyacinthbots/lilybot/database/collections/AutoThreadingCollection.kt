52c220d28f7ec246f9e1fdf5dc72585c6790185f
NoComment1105 <nocomment1105@outlook.com>
2022-11-02 19:19:21 +0000

    Add KDoc for the AutoThreadingCollection

--- src/main/kotlin/org/hyacinthbots/lilybot/database/collections/AutoThreadingCollection.kt
+++ src/main/kotlin/org/hyacinthbots/lilybot/database/collections/AutoThreadingCollection.kt
@@ -7,27 +7,74 @@ import org.hyacinthbots.lilybot.database.entities.AutoThreadingData
 import org.koin.core.component.inject
 import org.litote.kmongo.eq
 
-// todo kdoc
+/**
+ * This class contains the functions for interacting with the [AutoThreading Database][AutoThreadingData]. This
+ * class contains functions for getting, setting and removing auto threads channels.
+ *
+ * @since 4.4.0
+ * @see getAllAutoThreads
+ * @see getSingleAutoThread
+ * @see setAutoThread
+ * @see deleteAutoThread
+ * @see deleteGuildAutoThreads
+ */
 class AutoThreadingCollection : KordExKoinComponent {
 	private val db: Database by inject()
 
 	@PublishedApi
 	internal val collection = db.mainDatabase.getCollection<AutoThreadingData>()
 
+	/**
+	 * Gets all auto threads for a given [inputGuildId].
+	 *
+	 * @param inputGuildId The guild to get auto threads for
+	 * @return A list of all auto threads for the given guild
+	 * @author tempest15
+	 * @since 4.4.0
+	 */
 	suspend inline fun getAllAutoThreads(inputGuildId: Snowflake): List<AutoThreadingData> =
 		collection.find(AutoThreadingData::guildId eq inputGuildId).toList()
 
+	/**
+	 * Gets a single auto thread based off the channel ID.
+	 *
+	 * @param inputChannelId The ID of the channel to get
+	 * @return The [AutoThreadingData] for the channel
+	 * @author tempest15
+	 * @since 4.4.0
+	 */
 	suspend inline fun getSingleAutoThread(inputChannelId: Snowflake): AutoThreadingData? =
 		collection.findOne(AutoThreadingData::channelId eq inputChannelId)
 
-	suspend inline fun setAutoThread(inputAutoThreadData: AutoThreadingData) = run {
+	/**
+	 * Sets a new auto thread.
+	 *
+	 * @param inputAutoThreadData The Data for the new auto thread
+	 * @author tempest15
+	 * @since 4.4.0
+	 */
+	suspend inline fun setAutoThread(inputAutoThreadData: AutoThreadingData) {
 		collection.deleteOne(AutoThreadingData::channelId eq inputAutoThreadData.channelId)
 		collection.insertOne(inputAutoThreadData)
 	}
 
+	/**
+	 * Deletes an auto thread based off of the [inputChannelId].
+	 *
+	 * @param inputChannelId The channel to remove auto threading for
+	 * @author tempest15
+	 * @since 4.4.0
+	 */
 	suspend inline fun deleteAutoThread(inputChannelId: Snowflake) =
 		collection.deleteOne(AutoThreadingData::channelId eq inputChannelId)
 
+	/**
+	 * Deletes auto threads for a given guild.
+	 *
+	 * @param inputGuildId The guild to remove auto threads for
+	 * @author NoComment1105
+	 * @since 4.4.0
+	 */
 	suspend inline fun deleteGuildAutoThreads(inputGuildId: Snowflake) =
 		collection.deleteMany(AutoThreadingData::guildId eq inputGuildId)
 }
--- src/main/kotlin/org/hyacinthbots/lilybot/database/entities/AutoThreadingData.kt
+++ src/main/kotlin/org/hyacinthbots/lilybot/database/entities/AutoThreadingData.kt
@@ -6,16 +6,16 @@ import kotlinx.serialization.Serializable
 /**
  * The data for auto-threaded channels.
  *
- * @param guildId The ID of the guild the auto-threaded channel is in
- * @param channelId The ID of the auto-threaded channel being set
- * @param roleId The ID of the role to be mentioned in new threads
- * @param allowDuplicates If users should be allowed to have multiple threads in the channel
- * @param archive If threads should be archived upon creation
- * @param smartNaming If the threads should use a content-aware naming scheme
- * @param mention If the user who created the thread should be welcomed in the first message
- * @param creationMessage The message to send in the thread when it is created
+ * @property guildId The ID of the guild the auto-threaded channel is in
+ * @property channelId The ID of the auto-threaded channel being set
+ * @property roleId The ID of the role to be mentioned in new threads
+ * @property allowDuplicates If users should be allowed to have multiple threads in the channel
+ * @property archive If threads should be archived upon creation
+ * @property smartNaming If the threads should use a content-aware naming scheme
+ * @property mention If the user who created the thread should be welcomed in the first message
+ * @property creationMessage The message to send in the thread when it is created
  *
- * @since 4.1.0
+ * @since 4.4.0
  */
 @Serializable
 data class AutoThreadingData(
