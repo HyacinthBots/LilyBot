9692900ae98904be7718142f0012d3ec390972f6
NoComment1105 <nocomment1105@outlook.com>
2023-01-15 22:28:17 +0000

    Add code to ping status page

---
+++ src/main/kotlin/org/hyacinthbots/lilybot/extensions/util/StatusPing.kt
@@ -0,0 +1,35 @@
+package org.hyacinthbots.lilybot.extensions.util
+
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.utils.envOrNull
+import com.kotlindiscord.kord.extensions.utils.scheduling.Scheduler
+import com.kotlindiscord.kord.extensions.utils.scheduling.Task
+import io.ktor.client.HttpClient
+import io.ktor.client.request.post
+import mu.KotlinLogging
+import kotlin.time.Duration.Companion.seconds
+
+class StatusPing : Extension() {
+	override val name = "status-ping"
+
+	private val scheduler = Scheduler()
+
+	private lateinit var task: Task
+
+	private val client = HttpClient {}
+
+	private val env = envOrNull("STATUS_URL")
+
+	private val logger = KotlinLogging.logger("Status ping")
+
+	override suspend fun setup() {
+		task = scheduler.schedule(30.seconds, repeat = true, callback = ::post)
+	}
+
+	private suspend fun post() {
+		if (kord.gateway.gateways.isNotEmpty() && env != null) {
+			logger.debug { "Pinging!" }
+			client.post(env)
+		}
+	}
+}
