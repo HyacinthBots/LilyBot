94c4668f90f412125f0720c7cd40b78b9cf3121d
NoComment <nocomment1105@outlook.com>
2023-05-01 08:33:34 +0100

    Alter status ping code slightly

--- src/main/kotlin/org/hyacinthbots/lilybot/extensions/util/StatusPing.kt
+++ src/main/kotlin/org/hyacinthbots/lilybot/extensions/util/StatusPing.kt
@@ -1,12 +1,12 @@
 package org.hyacinthbots.lilybot.extensions.util
 
 import com.kotlindiscord.kord.extensions.extensions.Extension
-import com.kotlindiscord.kord.extensions.utils.envOrNull
 import com.kotlindiscord.kord.extensions.utils.scheduling.Scheduler
 import com.kotlindiscord.kord.extensions.utils.scheduling.Task
 import io.ktor.client.HttpClient
-import io.ktor.client.request.post
+import io.ktor.client.request.get
 import mu.KotlinLogging
+import org.hyacinthbots.lilybot.utils.ENV
 import kotlin.time.Duration.Companion.seconds
 
 class StatusPing : Extension() {
@@ -18,18 +18,18 @@ class StatusPing : Extension() {
 
 	private val client = HttpClient {}
 
-	private val env = envOrNull("STATUS_URL")
-
 	private val logger = KotlinLogging.logger("Status ping")
 
 	override suspend fun setup() {
-		if (env != null) {
+		if (ENV != null) {
 			task = scheduler.schedule(30.seconds, repeat = true, callback = ::post)
 		}
 	}
 
 	private suspend fun post() {
 		logger.debug { "Pinging!" }
-		client.post(env!!)
+		if (ENV != null) {
+			client.get(ENV)
+		}
 	}
 }
