8be156002e0158fbca37073523a337ef069db60c
NoComment <nocomment1105@outlook.com>
2023-08-15 06:10:52 +0100

    Remove Old Log-uploading DB Code

--- src/main/kotlin/org/hyacinthbots/lilybot/database/migrations/config/configV5.kt
+++ src/main/kotlin/org/hyacinthbots/lilybot/database/migrations/config/configV5.kt
@@ -1,24 +1,22 @@
-@file:Suppress("DEPRECATION_ERROR")
+@file:Suppress("DEPRECATION_ERROR", "UnusedPrivateMember", "UNUSED_PARAMETER", "RedundantSuspendModifier")
 
 package org.hyacinthbots.lilybot.database.migrations.config
 
-import org.hyacinthbots.lilybot.database.entities.UtilityConfigData
-import org.hyacinthbots.lilybot.database.entities.UtilityConfigDataOld
 import org.litote.kmongo.coroutine.CoroutineDatabase
 
 suspend fun configV5(db: CoroutineDatabase) {
-	val collection = db.getCollection<UtilityConfigDataOld>("utilityConfigData")
-	val oldConfigs = collection.find().toList()
-	val newConfigs = mutableListOf<UtilityConfigData>()
-
-	oldConfigs.forEach {
-		newConfigs.add(UtilityConfigData(it.guildId, it.utilityLogChannel))
-	}
-
-	db.dropCollection("utilityConfigData")
-	db.createCollection("utilityConfigData")
-
-	with(db.getCollection<UtilityConfigData>("utilityConfigData")) {
-		insertMany(newConfigs)
-	}
+// 	val collection = db.getCollection<UtilityConfigDataOld>("utilityConfigData")
+// 	val oldConfigs = collection.find().toList()
+// 	val newConfigs = mutableListOf<UtilityConfigData>()
+//
+// 	oldConfigs.forEach {
+// 		newConfigs.add(UtilityConfigData(it.guildId, it.utilityLogChannel))
+// 	}
+//
+// 	db.dropCollection("utilityConfigData")
+// 	db.createCollection("utilityConfigData")
+//
+// 	with(db.getCollection<UtilityConfigData>("utilityConfigData")) {
+// 		insertMany(newConfigs)
+// 	}
 }
