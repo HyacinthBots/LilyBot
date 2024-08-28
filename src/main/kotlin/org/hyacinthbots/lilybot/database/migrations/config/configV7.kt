3081b4596cdbb4e3a2c0cf726326d3f7572b8f20
NoComment <nocomment1105@outlook.com>
2024-08-28 14:26:48 +0100

    Update migrations for utility config

--- src/main/kotlin/org/hyacinthbots/lilybot/database/migrations/config/configV7.kt
+++ src/main/kotlin/org/hyacinthbots/lilybot/database/migrations/config/configV7.kt
@@ -1,6 +1,7 @@
 package org.hyacinthbots.lilybot.database.migrations.config
 
 import org.hyacinthbots.lilybot.database.entities.ModerationConfigData
+import org.hyacinthbots.lilybot.database.entities.UtilityConfigData
 import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.exists
 import org.litote.kmongo.setValue
@@ -10,6 +11,24 @@ suspend fun configV7(db: CoroutineDatabase) {
 		updateMany(
 			ModerationConfigData::autoInviteModeratorRole exists false,
 			setValue(ModerationConfigData::autoInviteModeratorRole, null)
+		)
+	}
+	with(db.getCollection<UtilityConfigData>("utilityConfigData")) {
+		updateMany(
+			UtilityConfigData::logChannelUpdates exists false,
+			setValue(UtilityConfigData::logChannelUpdates, false)
+		)
+		updateMany(
+			UtilityConfigData::logEventUpdates exists false,
+			setValue(UtilityConfigData::logEventUpdates, false)
+		)
+		updateMany(
+			UtilityConfigData::logInviteUpdates exists false,
+			setValue(UtilityConfigData::logInviteUpdates, false)
+		)
+		updateMany(
+			UtilityConfigData::logRoleUpdates exists false,
+			setValue(UtilityConfigData::logRoleUpdates, false)
 		)
 	}
 }
