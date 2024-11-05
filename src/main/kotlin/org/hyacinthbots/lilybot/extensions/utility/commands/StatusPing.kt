8cf0f740781c63d60146aeed1d9dda1ec9a59332
NoComment <nocomment1105@outlook.com>
2024-11-05 20:52:26 +0000

    Hopefully sort status ping out properly

--- src/main/kotlin/org/hyacinthbots/lilybot/extensions/utility/commands/StatusPing.kt
+++ src/main/kotlin/org/hyacinthbots/lilybot/extensions/utility/commands/StatusPing.kt
@@ -5,8 +5,8 @@ import dev.kordex.core.utils.scheduling.Scheduler
 import dev.kordex.core.utils.scheduling.Task
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.HttpClient
-import io.ktor.client.request.get
-import org.hyacinthbots.lilybot.utils.ENV
+import io.ktor.client.request.post
+import org.hyacinthbots.lilybot.utils.STATUS_ENV
 import kotlin.time.Duration.Companion.seconds
 
 class StatusPing : Extension() {
@@ -21,15 +21,15 @@ class StatusPing : Extension() {
 	private val logger = KotlinLogging.logger("Status ping")
 
 	override suspend fun setup() {
-		if (ENV != null) {
+		if (STATUS_ENV != null) {
 			task = scheduler.schedule(30.seconds, repeat = true, callback = ::post)
 		}
 	}
 
 	private suspend fun post() {
 		logger.debug { "Pinging!" }
-		if (ENV != null) {
-			client.get(ENV)
+		if (STATUS_ENV != null) {
+			client.post(STATUS_ENV)
 		}
 	}
 }
--- src/main/kotlin/org/hyacinthbots/lilybot/utils/_Constants.kt
+++ src/main/kotlin/org/hyacinthbots/lilybot/utils/_Constants.kt
@@ -22,6 +22,6 @@ val SENTRY_DSN = envOrNull("SENTRY_DSN")
 /** The environment the bot is being run in. production or development. */
 val ENVIRONMENT = env("ENVIRONMENT")
 
-val ENV = envOrNull("STATUS_URL")
+val STATUS_ENV = envOrNull("STATUS_URL")
 
 const val HYACINTH_GITHUB: String = "https://github.com/HyacinthBots"
