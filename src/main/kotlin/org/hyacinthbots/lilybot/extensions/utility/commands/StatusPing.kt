13e64eee0d5e0ea52b4a09990b27fb36c9437d7d
NoComment <nocomment1105@outlook.com>
2024-11-06 11:25:00 +0000

    Attempt again to fix the status ping

--- src/main/kotlin/org/hyacinthbots/lilybot/extensions/utility/commands/StatusPing.kt
+++ src/main/kotlin/org/hyacinthbots/lilybot/extensions/utility/commands/StatusPing.kt
@@ -1,12 +1,12 @@
 package org.hyacinthbots.lilybot.extensions.utility.commands
 
 import dev.kordex.core.extensions.Extension
+import dev.kordex.core.utils.envOrNull
 import dev.kordex.core.utils.scheduling.Scheduler
 import dev.kordex.core.utils.scheduling.Task
 import io.github.oshai.kotlinlogging.KotlinLogging
-import io.ktor.client.HttpClient
-import io.ktor.client.request.post
-import org.hyacinthbots.lilybot.utils.STATUS_ENV
+import io.ktor.client.*
+import io.ktor.client.request.*
 import kotlin.time.Duration.Companion.seconds
 
 class StatusPing : Extension() {
@@ -18,18 +18,18 @@ class StatusPing : Extension() {
 
 	private val client = HttpClient {}
 
+	private val statusEnv = envOrNull("STATUS_URL")
+
 	private val logger = KotlinLogging.logger("Status ping")
 
 	override suspend fun setup() {
-		if (STATUS_ENV != null) {
+		if (statusEnv != null) {
 			task = scheduler.schedule(30.seconds, repeat = true, callback = ::post)
 		}
 	}
 
 	private suspend fun post() {
 		logger.debug { "Pinging!" }
-		if (STATUS_ENV != null) {
-			client.post(STATUS_ENV)
+		client.post(statusEnv!!)
 		}
-	}
 }
